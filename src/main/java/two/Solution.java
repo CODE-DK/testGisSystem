c1c2cac5b30cf5c434b58f22f983ce5800109b46
Dmitriy Komovskiy <Dmitry.Komovskiy@x5.ru>
2019-08-21 17:16:54 +0300

    задание 2. Исправлено по уточнению

--- src/main/java/two/Solution.java
+++ src/main/java/two/Solution.java
@@ -1,17 +1,19 @@
 package two;
 
+import java.math.BigInteger;
+
 public class Solution {
 
-    private static int factorial(int x) {
-        int result = 1;
+    private static BigInteger factorial(int x) {
+        BigInteger result = BigInteger.valueOf(1L);
         for (int i = 2; i <= x; i++) {
-            result *= i;
+            result = result.multiply(BigInteger.valueOf(i));
         }
         return result;
     }
 
-    private static int f(int m, int r) {
-        return factorial(m) / factorial(r) * factorial(m - r);
+    private static BigInteger f(int m, int r) {
+        return factorial(m).divide(factorial(r).multiply(factorial(m - r)));
     }
 
     public static void main(String[] args) {
